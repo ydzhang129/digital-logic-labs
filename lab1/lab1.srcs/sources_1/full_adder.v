488f9a10621e733ebc47d904958b8964cf414d90
Zephyr Du <elecleus@outlook.com>
2026-05-21 18:01:50 +0800

    format all .v file

--- lab1/lab1.srcs/sources_1/full_adder.v
+++ lab1/lab1.srcs/sources_1/full_adder.v
@@ -1,11 +1,10 @@
 `timescale 1ns / 1ps
 
-module full_adder(
-    input   [7:0]   a,
-    input   [7:0]   b,
-    input           cin,
-    output  [7:0]   sum,
-    output          cout
-    );
-assign {cout, sum} = a + b + cin;
+module full_adder
+    (input [7 : 0] a,
+     input [7 : 0] b,
+     input cin,
+     output [7 : 0] sum,
+     output cout);
+    assign {cout, sum} = a + b + cin;
 endmodule
